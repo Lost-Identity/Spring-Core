fb3ef14aaa6a874fc99bad5cea8661d96f53038c
Ajay Patidar <patidarajay769@gmail.com>
2023-12-05 20:45:12 +0530

    we learned the reference injection

---
+++ springcore/src/main/java/com/springcore/reference/Test.java
@@ -0,0 +1,21 @@
+package com.springcore.reference;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/reference/Refconfig.xml");
+
+		A temp = (A) context.getBean("aref");
+		
+		System.out.println(temp.getX());
+		System.out.println(temp.getOb().getY());
+		
+		System.out.println(temp);
+	}
+
+}
